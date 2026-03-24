4cd1cd6af2813079345d7713c08be124215f1c25
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-24 21:11:14 +0530

    e:\Learn-javascript\JS-learning\OperatorConditionalStatments\swap.js

---
+++ OperatorConditionalStatments/swap.js
@@ -0,0 +1,18 @@
+// let a = 6;
+// let b = 5;
+
+// a = a + b;  // 6+ 5 = 11
+// b = a - b; //  11 - 5 = 6
+// a = a - b; // 11 - 6 = 5
+
+// console.log(a,b);
+
+
+let a= 5;
+let b = 6;
+
+a = a*b;
+b= a/b;
+
+a = a/b;
+console.log(a,b);
