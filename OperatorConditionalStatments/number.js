4d252fe6d38e148592d6fe5facd7c4542dd90509
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-24 21:08:37 +0530

    e:\Learn-javascript\JS-learning\OperatorConditionalStatments\number.js

---
+++ OperatorConditionalStatments/number.js
@@ -0,0 +1,8 @@
+let a = 12;
+let b = 10;
+
+console.log(" a+ b = ", a + b);
+console.log(" a-b = ", a - b);
+console.log(" a*b = ", a * b);
+console.log(" a/b = ", a / b);
+console.log(" a%b = ", a % b);
