8cc9b28afbae3b00098040d861978f118f264979
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-25 23:28:54 +0530

    e:\Learn-javascript\JS-learning\OperatorConditionalStatments\string.js

---
+++ OperatorConditionalStatments/string.js
@@ -0,0 +1,100 @@
+// let str = "apnadunia";
+
+// console.log(str[0]);
+// console.log(str[1]);
+// console.log(str[2]);
+// console.log(str[3]);
+// console.log(str[4]);
+// console.log(str[5]);
+// console.log(str[6]);
+// console.log(str[7]);
+// console.log(str[8]);
+
+
+//template literals in js
+
+// let specialstring = `This is a template literals`;
+// console.log(typeof specialstring);
+
+
+// let obj ={
+//     item: "pen",
+//     price : 10,
+// };
+// console.log(" The cost of " ,obj.item , " is", obj.price ," rupess");
+
+
+//template literals is used
+
+// let specialstring = `This is a template literals ${1+2+3+4+5+6+7+8+9+0}`;
+// console.log(specialstring);
+
+
+// let obj = {
+//     item : " pencil",
+//     cost : 10,
+
+// };
+
+// let output = `The cost of ${obj.item} is ${obj.cost} rupees`;
+// console.log(output);
+
+
+//escape character
+
+// console.log("apna\ndunia");
+// console.log("apna\tdunia");
+
+// let str = "apna\ndunia";
+// console.log(str.length);
+
+
+
+// built in function  toUpperCase
+
+// let str = " apna dunia";
+// str = str.toUpperCase();
+// console.log(str);
+
+
+
+// built in function  toLowerCase
+
+// let str = " APNA DUNIYA";
+// str = str.toLowerCase();
+// console.log(str);
+
+// using str.trim method
+
+// let str = "    javascript most popular    ";
+// console.log(str.trim());
+
+// using str.slice method
+// let str = "0123456789";
+// console.log(str.slice(1,8));
+// let  str = " Hello";
+
+// console.log(str.slice(1,5));
+
+
+
+// using str.concat method
+
+// let str1 = " Hello";
+// let str2 = " World";
+
+// let res = str2.concat(str1);
+
+// console.log(res);
+
+
+// using str.replace(searchVal , newVal)
+
+// let str = " Hello";
+// console.log(str.replace("lo", "p"));
+
+// using str.CharAt(idx)
+
+let str = " I love JS";
+console.log(str.charAt(3));
+
