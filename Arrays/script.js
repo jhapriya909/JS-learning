6df05f7c4807dac8d33787428b5ffd158e2e3893
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-27 20:25:45 +0530

    e:\Learn-javascript\JS-learning\Arrays\script.js

---
+++ Arrays/script.js
@@ -0,0 +1,8 @@
+let marks = [35, 56, 79, 89, 90];
+console.log(marks);
+console.log(marks.length);
+
+
+let herores = [" ironman", "bulk", "thor", "batman", "antman", "bulkman"];
+console.log(herores);
+console.log(herores.length);
