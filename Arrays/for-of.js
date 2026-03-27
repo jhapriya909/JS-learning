00a53f4aa6967637087a664f845030f6bc14a777
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-27 20:30:20 +0530

    e:\Learn-javascript\JS-learning\Arrays\for-of.js

---
+++ Arrays/for-of.js
@@ -0,0 +1,11 @@
+// let heroes = [" ironman", "bulk", "thor", "batman", "antman", "bulkman"];
+
+// for(let hero of heroes){
+//     console.log(hero);
+// }
+
+let citizes = ["delhi", "jaipur", "rajkot", "mumbai","hyderabad", "noida"];
+
+for(let city of citizes){
+    console.log(city.toUpperCase());
+}
