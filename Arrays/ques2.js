a7fa9d8eb756a859d14c8468b3c84ec2cb0ef3d3
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-27 20:33:10 +0530

    e:\Learn-javascript\JS-learning\Arrays\ques2.js

---
+++ Arrays/ques2.js
@@ -0,0 +1,15 @@
+let prices = [250, 645,300, 900, 50];
+
+
+let idx = 0;
+
+for(let  val of prices){
+
+    console.log(`value at index ${idx} = ${val}`);
+    idx++;
+
+    offer = prices %10;
+
+    val = offer - prices
+
+}
