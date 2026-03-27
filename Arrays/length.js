286ee5af4d1a14cc00f7ce8fb6084134b184bab1
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-27 20:29:14 +0530

    e:\Learn-javascript\JS-learning\Arrays\length.js

---
+++ Arrays/length.js
@@ -0,0 +1,14 @@
+let heroes = [" ironman", "bulk", "thor", "batman", "antman", "bulkman"];
+
+
+// for(let idx = 0; idx<heroes.length; idx++){
+//     console.log(heroes[idx]);
+// }
+
+for(let i = 0; i<heroes.length; i++){
+    console.log(heroes[i]);
+}
+// i = 0; i<idx.length
+// i = 1; i<=idex.length
+
+
