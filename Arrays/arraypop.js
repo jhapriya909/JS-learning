45a4ceb6cd66765e1f3cd83227c5f0f35f76c067
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-28 22:55:45 +0530

    e:\Learn-javascript\JS-learning\Arrays\arraypop.js

---
+++ Arrays/arraypop.js
@@ -0,0 +1,7 @@
+let foodItems = ["patato"," apple", "grapes", "orange", "lemon"];
+console.log(foodItems);
+
+let deletedItem = foodItems.pop();
+console.log(foodItems);
+
+console.log("deleted" , deletedItem);
