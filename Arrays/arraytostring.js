83af5650d828a92c4d91e4894342e21e7fc06178
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-29 21:28:38 +0530

    e:\Learn-javascript\JS-learning\Arrays\arraytostring.js

---
+++ Arrays/arraytostring.js
@@ -0,0 +1,6 @@
+let foodItems = ["patato"," apple", "grapes", "orange", "lemon"];
+
+let marks = [34,67,78,98,90,42];
+console.log(foodItems.toString());
+console.log(foodItems);
+console.log(marks);
