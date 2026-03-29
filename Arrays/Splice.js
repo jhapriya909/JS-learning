0ed153fe96a1bbe2295c9804f73df07c13e5dce4
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-29 21:30:52 +0530

    e:\Learn-javascript\JS-learning\Arrays\Splice.js

---
+++ Arrays/Splice.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5,6,7,8];
+// arr.splice(2,2, 101, 102);
+
+// add element 
+// arr.splice(2,0, 101);
+
+// delete element
+// arr.splice(2,3);
+
+// replace element 
+arr.splice(3,1 ,101);
+
