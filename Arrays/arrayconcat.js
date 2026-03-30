adad932bb8556872aabcaca9672def548210982e
jhapriya909 <thepriyajha8840@gmail.com>
2026-03-30 22:29:18 +0530

    e:\Learn-javascript\JS-learning\Arrays\arrayconcat.js

---
+++ Arrays/arrayconcat.js
@@ -0,0 +1,8 @@
+let marvelHouse = ["ironman", "spiderman", "thor"];
+
+let dcHouse = [" superman", "batman"];
+let indianHeroes = [" saktiman", " krish"];
+
+let result = marvelHouse.concat(dcHouse , indianHeroes);
+
+console.log(result);
