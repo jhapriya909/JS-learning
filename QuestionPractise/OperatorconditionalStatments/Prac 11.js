0ae864681997add18bec763782c332f32ee7bb0e
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-03 21:20:01 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 11.js
@@ -0,0 +1,10 @@
+// Ternary Operator
+// Input: 10  
+// Output: "Even"
+
+// condition ? expressionIfTrue : expressionIfFalse;
+
+let n = 10;
+
+let result = (n%2==0) ? "even" : "odd";
+console.log(result);
