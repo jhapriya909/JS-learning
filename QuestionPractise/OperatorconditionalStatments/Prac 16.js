e89909e7e19318abd42417f622c9dd7d61584e6f
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-09 15:29:12 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 16.js
@@ -0,0 +1,23 @@
+//Input: a = 10, b = 5, op = "+" Output: 15
+
+// let a = 10;
+// let b = 5;
+// let op = "-";
+// let result;
+
+// if(op === "-"){
+//     result = (a-b);
+// }
+
+// console.log(result);
+
+
+let a = 10;
+let b = 20;
+let op = "*";
+let result;
+
+if(op === "*"){
+    result = (a*b);
+}
+console.log(result);
