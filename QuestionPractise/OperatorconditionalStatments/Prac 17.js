b444c9bf7135d03e00f41d9baa6f4c3a81c1a417
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-09 15:51:06 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 17.js
@@ -0,0 +1,16 @@
+// console.log(5 == "5"); // output true
+// console.log(5 === "5"); // output false
+
+// console.log(true + false); // true -->1
+
+
+// let a = 10;
+// console.log(a++); // 11
+// console.log(++a); // 12
+
+
+console.log(0 == false);  // false
+console.log(0 === false); // true
+
+
+console.log(5 > 3 && 2 < 1); // false
---
+++ QuestionPractise/OperatorconditionalStatments/Prac 18.js
@@ -0,0 +1,23 @@
+// Ek number lo aur check karo:
+// divisible by 2
+// divisible by 3
+// done
+// none
+
+let  a = 12;
+
+if(a%2 === 0 && a%3 === 0){
+    console.log(" both are equal")
+}
+
+else if (a%2 == 0)
+{
+     console.log(" divisible by 2")
+}
+else if(a%3 === 0){
+      console.log(" divisible by 3")
+}
+
+else{
+    console.log("none");
+}
