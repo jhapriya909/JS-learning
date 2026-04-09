a56a3d48adae74d883d146699b5ed71f00531ee9
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-09 15:56:01 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 19.js
@@ -0,0 +1,19 @@
+
+// age < 18 → "Minor"
+// 18–60 → "Adult"
+
+// 60 → "Senior"
+
+
+let age = 18;
+
+if (age < 18) {
+    console.log("Minor");
+
+}
+else if (age >= 18 && age <= 60) {
+    console.log("Adult")
+}
+else {
+    console.log(" Senior");
+}
