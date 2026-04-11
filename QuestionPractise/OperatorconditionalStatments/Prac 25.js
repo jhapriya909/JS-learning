2e271994f7ac1726fc2c674063794282234c8e5a
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-11 21:37:07 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 25.js
@@ -0,0 +1,14 @@
+//Divisible check
+// Number check karo:
+// divisible by 5 → "Yes"
+// else → "No"
+
+
+let number = 15;
+
+if(number%5 === 0){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
