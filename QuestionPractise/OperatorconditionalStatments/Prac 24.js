8e35571a2c8a5f2ac2189d59106d02e859f69e81
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-11 21:32:34 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 24.js
@@ -0,0 +1,13 @@
+// Voting eligibility
+//  age input  ≥ 18 → "Can Vote"
+// else → "Cannot Vote"
+
+let age = 10;
+
+if(age >= 18){
+    console.log("Can Vote");
+}
+
+else{
+    console.log("Cannot Vote");
+}
