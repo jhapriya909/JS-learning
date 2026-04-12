051c8d3627c09cb40e60865d7b04d0f162841c81
jhapriya909 <thepriyajha8840@gmail.com>
2026-04-12 21:34:25 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\OperatorconditionalStatments

---
+++ QuestionPractise/OperatorconditionalStatments/Prac 27.js
@@ -0,0 +1,19 @@
+// Greatest of 4 numbers
+// find 4 largest  numbers 
+let l1 = 10;
+let l2 = 20;
+let l3 = 57;
+let l4 = 34;
+
+if (l1 >= l2 && l1 >= l3 && l1 >= l4) {
+    console.log(l1);
+} 
+else if (l2 >= l1 && l2 >= l3 && l2 >= l4) {
+    console.log(l2);
+} 
+else if (l3 >= l1 && l3 >= l2 && l3 >= l4) {
+    console.log(l3);
+} 
+else {
+    console.log(l4);
+}
