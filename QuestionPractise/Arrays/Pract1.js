97035982aeaba4435807ed8ed0544b4736bcdef9
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-02 21:20:26 +0530

    e:\Learn-javascript\JS-learning\QuestionPractise\Arrays

---
+++ QuestionPractise/Arrays/Pract1.js
@@ -0,0 +1,7 @@
+let companies = ["Bloomberg", " Microsoft", " Uber", " Google", " IBM", " Netflix"]
+
+//  (a)// companies.shift();
+
+// (b)// companies.splice(2 , 1, "Ola");
+
+companies.push("Amazon");
