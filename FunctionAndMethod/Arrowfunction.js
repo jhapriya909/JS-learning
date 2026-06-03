e8b5927bd16c773ca2375f56c729139e0fd265b6
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-03 20:28:31 +0530

    e:\Learn-javascript\JS-learning\FunctionAndMethod\Arrowfunction.js

---
+++ FunctionAndMethod/Arrowfunction.js
@@ -0,0 +1,23 @@
+const arrowSum=(a,b)=>{
+    return a + b;
+
+
+}
+
+const arrowSub=(a,b)=>{
+    return a-b;
+}
+
+
+const arrowMul=(a,b)=>{
+    return a*b;
+}
+
+const arrowDiv=(a,b)=>{
+    return a/b; // console also used 
+}
+
+
+const printworld=()=>{
+    console.log("hello world");
+}
