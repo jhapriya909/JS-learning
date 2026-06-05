b1fd76d7e38350acdb8ea3ec0bab2a00e88fe6bc
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-05 21:15:21 +0530

    e:\Learn-javascript\JS-learning\FunctionAndMethod\squareeachfor.js

---
+++ FunctionAndMethod/squareeachfor.js
@@ -0,0 +1,16 @@
+// let nums = [2,3,4,5,6,7]
+// nums.forEach((num) =>{
+
+//     console.log(num*num);
+
+// })
+
+// second method
+
+let nums = [2,6,78,9]
+
+let calcSquare = (num) =>{
+    console.log(num*num);
+};
+
+nums.forEach(calcSquare);
