ca7a2cec35de01cb0c668ac541add093aab7f148
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-05 21:12:09 +0530

    e:\Learn-javascript\JS-learning\FunctionAndMethod\callback.js

---
+++ FunctionAndMethod/callback.js
@@ -0,0 +1,20 @@
+// let arr = [1,3,4,5,6,7]
+// arr.forEach(function printVal(val){ // value at each idx
+//     console.log(val);
+// })
+
+// arrow fnction used
+
+
+// let arr = [1,2,3,4,5]
+// arr.forEach((val)=>{
+//     console.log(val);
+
+// })
+
+let arr =[" pune", "delhi", "patna", "jharkhand"]
+
+arr.forEach((val,idx,arr)=>{
+    console.log(val.toUpperCase(),idx,arr);
+
+})
