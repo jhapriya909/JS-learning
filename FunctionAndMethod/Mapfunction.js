bd48c5b357d99101d1e40f94eee749c90b4b9bfc
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-05 21:14:03 +0530

    e:\Learn-javascript\JS-learning\FunctionAndMethod\Mapfunction.js

---
+++ FunctionAndMethod/Mapfunction.js
@@ -0,0 +1,18 @@
+// let nums = [1,2,3,4]
+
+// let newArr = nums.map((val)=>{
+//     return val;
+
+// })
+
+
+let nums = [45,78,89,90]
+
+let newArr = nums.map((val) => {
+
+    // return val * val;
+
+    return val * 2;
+
+});
+console.log(newArr);
