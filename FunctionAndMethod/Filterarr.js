ba7bf2239e5ccf35cbe94f6208a525e9b83918f3
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-05 21:16:28 +0530

    e:\Learn-javascript\JS-learning\FunctionAndMethod\Filterarr.js

---
+++ FunctionAndMethod/Filterarr.js
@@ -0,0 +1,49 @@
+// let arr = [1,2,3,4, 5,6,7,8,9,0]
+
+// let evenArr = arr.filter((val)=>
+// {
+//  return val % 2 === 0
+
+// });
+
+// console.log(evenArr);
+
+
+
+// let arr = [1,2,3,4, 5,6,7,8,9,0]
+
+// let evenArr = arr.filter((val)=>
+// {
+//  return val % 2!== 0
+
+// });
+
+// console.log(evenArr);
+
+
+// let arr = [1,2,3,4, 5,6,7,8,9,0]
+
+// let evenArr = arr.filter((val)=>
+// {
+//  return val > 2
+
+// });
+
+// console.log(evenArr);
+
+
+let arr = [1,2,3,4, 5,6,7,8,9,0]
+
+let evenArr = arr.filter((val)=>
+{
+ return val <3
+
+});
+
+console.log(evenArr);
+
+
+
+
+
+
