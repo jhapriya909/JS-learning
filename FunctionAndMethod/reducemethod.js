4c9b91255ca943b0777da8ba567403c1c20fb538
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-06 20:46:51 +0530

    e:\Learn-javascript\JS-learning\FunctionAndMethod\reducemethod.js

---
+++ FunctionAndMethod/reducemethod.js
@@ -0,0 +1,25 @@
+let n = prompt("Enter a numbers");
+
+let arr = [];
+
+for(let i = 1;i<=n; i++ ){
+    arr[i-1] = i;
+}
+
+console.log(arr);
+
+
+ let sum = arr.reduce((res,curr)=>{
+    return res + curr;
+
+})
+
+console.log(" sum : " ,sum);
+
+
+let factorial = arr.reduce((res, curr)=>{
+
+    return res * curr;
+})
+
+console.log(factorial)
