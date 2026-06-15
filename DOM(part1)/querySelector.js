7fafec9c048f09f92c1008e4decaf167bc2455c1
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-15 22:29:53 +0530

    e:\Learn-javascript\JS-learning\DOM(part1)\querySelector.js

---
+++ DOM(part1)/querySelector.js
@@ -0,0 +1,24 @@
+// let elements = document.querySelector("p");
+// console.dir(elements);
+
+
+// let firstEl = document.querySelector("p");
+// console.dir(firstEl);
+
+// // let allEl = document.querySelectorAll("p")// all element
+// console.dir(allEl);
+
+// let firstEl = document.querySelector(".myclass");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("p")// all element
+// console.dir(allEl);
+
+
+let elements = document.querySelector("#myId");
+console.dir(elements);
+
+
+
+
+
