b681af4126bab62edb253bd724899c0bc737b341
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-18 20:26:16 +0530

    e:\Learn-javascript\JS-learning\DOM(part1)\tagName.js

---
+++ DOM(part1)/tagName.js
@@ -0,0 +1,32 @@
+// let elements = document.querySelector("p");
+// console.dir(elements);
+
+
+// let firstEl = document.querySelector("p");
+// console.dir(firstEl);
+
+// // let allEl = document.querySelectorAll("p")// all element
+// console.dir(allEl);
+
+// let firstEl = document.querySelector(".myclass");
+// console.dir(firstEl);
+
+// let allEl = document.querySelectorAll("p")// all element
+// console.dir(allEl);
+
+
+// let elements = document.querySelector("#myId");
+// console.dir(elements);
+
+
+// //nodes
+// console.dir(document.body.firstChild);
+
+
+
+let div = document.querySelector("div");
+console.dir(div);
+
+
+let heading = document.querySelector("h1")
+
