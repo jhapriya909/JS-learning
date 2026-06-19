88fb0c8d7ad5dbe158808310bca07c42d5206754
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-19 23:08:37 +0530

    e:\Learn-javascript\JS-learning\DOM(part1)\nodestyle.js

---
+++ DOM(part1)/nodestyle.js
@@ -0,0 +1,8 @@
+let box = document.getElementById("box");
+
+    box.style.width = "200px";
+    box.style.height = "200px";
+    box.style.backgroundColor = "lightpink";
+    box.style.border = "2px solid black";
+    box.style.textAlign = "center";
+    box.style.lineHeight = "200px";
