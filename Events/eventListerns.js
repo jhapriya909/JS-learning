63b3b07b02b3ce68efc7ddd66b9bf1c946b17eb4
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-20 22:06:49 +0530

    e:\Learn-javascript\JS-learning\Events\eventListerns.js

---
+++ Events/eventListerns.js
@@ -0,0 +1,22 @@
+
+btn1.addEventListener("click",(evt) =>{
+    console.log("button was clicked - handler1")
+});
+
+btn1.addEventListener("click",() =>{
+    console.log("button was clicked - handler2")
+});
+
+btn1.addEventListener("click",() =>{
+    console.log("button was clicked - handler3")
+});
+
+
+btn1.addEventListener("click",() =>{
+    console.log("button was clicked - handler4")
+});
+
+
+
+
+
