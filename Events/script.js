2d5dbd9f65820e69a633c25c1d7c210bd50e99a7
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-22 23:19:46 +0530

    e:\Learn-javascript\JS-learning\Events\script.js

---
+++ Events/script.js
@@ -0,0 +1,21 @@
+let btn1 = document.querySelector("#btn1");
+
+btn1.onclick = () => {
+    console.log("button was clicked");
+
+}
+
+
+let btn2 = document.querySelector("#btn2");
+
+btn2.onclick=() => {
+    console.log(" button clicked here 2x speed")
+}
+
+
+let div = document.querySelector("div");
+
+div.onmouseover =()=>{
+    console.log (" over the button click here!");
+}
+   
