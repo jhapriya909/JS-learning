19cf4205224f42b8ca0f8e5e81e43d0acb39c586
jhapriya909 <thepriyajha8840@gmail.com>
2026-06-23 21:36:23 +0530

    e:\Learn-javascript\JS-learning\DOM(part1)\pra4.js

---
+++ DOM(part1)/pra4.js
@@ -0,0 +1,15 @@
+let para = document.querySelector("p");
+//
+
+// para
+// <p class=​"newClass newclass">​…​</p>​
+// para.getAttribute("class")
+// 'content'
+// para.setAttribute("class","newClass")
+// undefined
+// para.classList
+// DOMTokenList ['newClass', value: 'newClass']0: "newClass"length: 1value: "newClass"[[Prototype]]: DOMTokenList
+// para.classList.add("newclass");
+// undefined
+// para.classList
+// DOMTokenList(2) ['newClass', 'newclass', value: 'newClass newclass']0: "newClass"1: "newclass"length: 2value: "newClass newclass"[[Prototype]]: DOMTokenList
