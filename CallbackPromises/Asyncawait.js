8b6d77223d992bbdd21218ac1adabfb5c99df2e0
jhapriya909 <thepriyajha8840@gmail.com>
2026-07-06 23:08:00 +0530

    e:\Learn-javascript\JS-learning\CallbackPromises\Asyncawait.js

--- CallbackPromises/Asyncawait.js
+++ CallbackPromises/Asyncawait.js
@@ -1,13 +1,70 @@
-  async function hello (){
-    console.log(" Hello")
+//   async function hello (){
+//     console.log(" Hello")
+
+// }
+
+// const getPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         console.log("I am a promise");
+
+//         resolve("success");
+//         reject("network error");
+//     });
+// };
+
+
+function api(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(" whether data");
+            resolve(200);
+
+        },2000);
+    });
+}
+
+async function getWhetherData(){
+
+   await api();
+   await api();
+}
+
+
+
+function getData(dataId){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log(" data",dataId);
+            resolve("success");
+
+        },2000);
+    });
+}
+
+async function getAllData(){
+     console.log(" getting data1");
+    await getData(1);
+   console.log("getting  data2");
+     await getData(2);
+        console.log("getting  data3");
+      await getData(3);
+         console.log("getting  data4");
 
 }
 
-const getPromise = () => {
-    return new Promise((resolve, reject) => {
-        console.log("I am a promise");
+//IIFE ---> automatically excute code
 
-        resolve("success");
-        reject("network error");
-    });
-};
+(async function(){
+     console.log(" getting data1");
+    await getData(1);
+   console.log("getting  data2");
+     await getData(2);
+        console.log("getting  data3");
+      await getData(3);
+         console.log("getting  data4");
+
+})();
+
+
+
+
