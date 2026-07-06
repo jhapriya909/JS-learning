e9b166869a589249debe3c440facba12410df6c8
jhapriya909 <thepriyajha8840@gmail.com>
2026-07-06 23:10:16 +0530

    e:\Learn-javascript\JS-learning\CallbackPromises\callbackhell.js

---
+++ CallbackPromises/callbackhell.js
@@ -0,0 +1,22 @@
+function getData (dataId,getNextData){
+    setTimeout(()=>{
+        console.log(" Data", dataId);
+        if(getNextData){
+            getNextData();
+        }
+
+    },2000)
+}
+
+// callback hell 
+getData(1,()=>{
+    console.log(" getting data2....");
+
+    getData(2,()=>{
+          console.log(" getting data3....");
+    })
+    getData(3,()=>{
+          console.log(" getting data4....");
+    })
+
+})
